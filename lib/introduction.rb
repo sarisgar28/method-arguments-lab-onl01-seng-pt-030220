0aad6d06dfec43d140121b6b67f834ea1ca00c6f
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-03-02 20:23:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-def introduction_with_name (name,language)
+def introduction_with_name_and_language (name,language)
   puts.phrase ("Hi, my name is Dan #{name} and I am learning to program #{language}.") do 
  end 
  
